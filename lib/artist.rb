154612b4591280f3f769f0ee6eb88a571aecaab2
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-06 06:20:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,13 +14,9 @@ class Artist
     @songs = []
   end
 
-  
-
   def self.all
     @@artists
   end
-
-  
 
   def add_song(song)
     @songs << song
@@ -30,8 +26,4 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
